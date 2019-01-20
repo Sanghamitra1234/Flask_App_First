21d92c0ca8b24563a61027518b73fff4613a9777
Sanghamitra1234 <sanghota4567@gmail.com>
2019-01-20 17:06:32 +0530

    edited

--- deploy.py
+++ deploy.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def index():
-	return '<h1>Hello, This is Sanghamitra!!!</h1>
+	return '<h1>Hello, This is Sanghamitra!!!</h1>'
 
 if__name__=='__main__':
 	app.run()
